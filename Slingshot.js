8ea63d5d0ddf4225df7b826af2ed60388ebc509a
Your Name <you@example.com>
2020-10-12 16:03:12 +0530

    vanish

--- Slingshot.js
+++ Slingshot.js
@@ -13,7 +13,12 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+attach(bodyA){
+this.sling.bodyA=bodyA;
 
+
+
+}
     fly(){
         this.sling.bodyA = null;
     }
